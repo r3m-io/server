6f80535a3ba4a515da4bec1781b42c13bc2b9241
remco van der Velde <remco.pc@outlook.com>
2023-12-26 18:45:54 +0100

    public create

--- src/Package/Trait/Server.php
+++ src/Package/Trait/Server.php
@@ -1,7 +1,9 @@
 <?php
 namespace Package\R3m\Io\Server\Trait;
 
+use R3m\Io\Config;
 use R3m\Io\Module\Core;
+use R3m\Io\Module\Event;
 
 use Exception;
 
@@ -11,12 +13,34 @@ trait Server {
 
     /**
      * @throws ObjectException
+     * @throws Exception
      */
     public function public_create($options): void
     {
         $object = $this->object();
         $options = Core::object($options, Core::OBJECT_ARRAY);
-
+        $id = $object->config(Config::POSIX_ID);
+        if(
+            !in_array(
+                $id,
+                [
+                    0,
+                    33
+                ],
+                true
+            )
+        ){
+            $exception = new Exception('Only root and after that www-data can configure public create...');
+            Event::trigger($object, 'cli.configure.public.create', [
+                'options' => $options,
+                'exception' => $exception
+            ]);
+            throw $exception;
+        }
+        if(!array_key_exists('public', $options)){
+            $options['public'] = 'Public';
+        }
+        d($object->config('project'));
         ddd($options);
         /*
         $package = $object->request('package');
