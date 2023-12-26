a7d858c98e24d80e2f21336300a03c4bc5f2ace4
remco van der Velde <remco.pc@outlook.com>
2023-12-26 18:25:31 +0100

    public create

--- src/Package/Trait/Server.php
+++ src/Package/Trait/Server.php
@@ -1,19 +1,17 @@
 <?php
 namespace Package\R3m\Io\Server\Trait;
 
-use R3m\Io\App;
-
 use R3m\Io\Module\Core;
-use R3m\Io\Module\File;
-
-use R3m\Io\Node\Model\Node;
 
 use Exception;
+
 trait Server {
 
     public function public_create($options): void
     {
         $object = $this->object();
+        $options = Core::object($options, Core::OBJECT_ARRAY);
+
         ddd($options);
         /*
         $package = $object->request('package');
