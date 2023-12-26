aaf6ea122cd01e8f1d5b1c518592f65079b06756
remco van der Velde <remco.pc@outlook.com>
2023-12-26 19:19:23 +0100

    public create

--- src/Package/Trait/Server.php
+++ src/Package/Trait/Server.php
@@ -3,6 +3,7 @@ namespace Package\R3m\Io\Server\Trait;
 
 use R3m\Io\Config;
 use R3m\Io\Module\Core;
+use R3m\Io\Module\Dir;
 use R3m\Io\Module\Event;
 
 use Exception;
@@ -43,6 +44,16 @@ trait Server {
         if(strstr($options['public'], '/') === false){
             $options['public'] = $object->config('project.dir.root') . $options['public'] . $object->config('ds');
         }
+        $source = $object->config('controller.dir.data') . 'Server' . $object->config('ds');
+        $destination = $options['public'];
+        Dir::create($destination, Dir::CHMOD);
+        Dir::copy($source, $destination);
+        File::permission($object, [
+            'destination' => $destination,
+            '.htaccess' => $destination . '.htaccess',
+            '.user.ini' => $destination . '.user.ini',
+            'index.php' => $destination . 'index.php',
+        ]);
 d($object->config('controller.dir.data'));
 //        d($object->config('project'));
         ddd($options);
